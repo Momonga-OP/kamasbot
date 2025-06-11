8cabe954d0b9e70779fdf4cb2ab53a4e93d1b008
Momonga-OP <latifdofus@gmail.com>
2025-06-11 21:27:28 +0100

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,13 +1,8 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # Channel IDs
-PANEL_CHANNEL_ID = int(os.getenv('PANEL_CHANNEL_ID'))
-TICKET_CHANNEL_ID = int(os.getenv('TICKET_CHANNEL_ID'))
-VERIFIED_DATA_CHANNEL_ID = int(os.getenv('VERIFIED_DATA_CHANNEL_ID'))
-SERVER_ID = int(os.getenv('SERVER_ID'))
+PANEL_CHANNEL_ID = 1234567890  # Replace with your actual channel ID
+TICKET_CHANNEL_ID = 1234567890  # Replace with your actual channel ID
+VERIFIED_DATA_CHANNEL_ID = 1234567890  # Replace with your actual channel ID
+SERVER_ID = 1234567890  # Replace with your actual server ID
 
 # Kamas logo URL
 KAMAS_LOGO_URL = "https://static.wikia.nocookie.net/dofus/images/1/1e/Kama.png"
