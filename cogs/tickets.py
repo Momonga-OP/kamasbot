ee04dd0f6b32e0b7931736b5ad572cfe600dbadd
Momonga-OP <your-email@example.com>
2025-06-14 01:08:52 +0100

    Fixed app_commands import location

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import app_commands
 from discord import ui
 from discord import app_commands
 from datetime import datetime
